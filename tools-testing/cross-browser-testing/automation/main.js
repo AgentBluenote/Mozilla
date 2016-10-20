93db14f3694da7e530852b545c1d7dc28c4edab6
chrisdavidmills <chrisdavidmills@gmail.com>
2016-10-20 15:10:12 +0100

    adding automated testing samples

---
+++ tools-testing/cross-browser-testing/automation/main.js
@@ -0,0 +1,8 @@
+var array = ['Chris', 'Bob', 'Mark', 'Paul'];
+
+// Arrow function syntax
+array.forEach((e, i, a) => {
+   var elem = document.createElement('p');
+   elem.textContent = (i + 1) + '. ' + e;
+   document.body.appendChild(elem);
+});
