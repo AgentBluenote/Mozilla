ba404e5a22a908ff94738c6d7a31cb14d02b1408
chrisdavidmills <chrisdavidmills@gmail.com>
2018-01-15 20:17:24 +0000

    adding cache and service worker example

--- javascript/apis/client-side-storage/cache-sw/video-store-offline/index.js
+++ javascript/apis/client-side-storage/cache-sw/video-store-offline/index.js
@@ -138,4 +138,12 @@ function storeVideo(mp4Blob, webmBlob, name) {
 
     console.log('Database setup complete');
   };
+
+  // Register service worker to control making site work offline
+
+  if('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('/sw.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
 };
