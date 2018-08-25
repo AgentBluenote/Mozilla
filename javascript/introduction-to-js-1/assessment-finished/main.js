778ee12a51335794df96f73c1e0a6c4f764a1ba9
LittleMang <42237608+LittleMang@users.noreply.github.com>
2018-08-25 22:50:49 +0800

    My question
    I am confused, please compare the contents of these two URLs.
    10 lines ：It was 94 fahrenheit 
    https://github.com/mdn/learning-area/edit/master/javascript/introduction-to-js-1/assessment-start/raw-text.txt
    13 lines：It was 94 farenheit
    These two are different,I am using the Chinese tutorial at MDN
    将 '94 farenheit' 置换成为变量 temperature 的值
    According to the prompt I copied '94 farenheit',then an error occurred, I spent an hour finding out the problem.
    This may cause problems for other learners.

--- javascript/introduction-to-js-1/assessment-finished/main.js
+++ javascript/introduction-to-js-1/assessment-finished/main.js
@@ -7,7 +7,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-var storyText = 'It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+var storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 var insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
 var insertY = ['the soup kitchen','Disneyland','the White House'];
 var insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
