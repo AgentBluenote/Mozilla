80a775db207e58e51122d344910515f9b797c96c
Xu Chunyang <mail@xuchunyang.me>
2018-09-19 02:26:47 +0800

    Fix undefined variable

--- javascript/apis/fetching-data/can-store/can-script.js
+++ javascript/apis/fetching-data/can-store/can-script.js
@@ -153,7 +153,7 @@ function initialize() {
         response.blob().then(function(blob) {
           // Convert the blob to an object URL — this is basically an temporary internal URL
           // that points to an object stored inside the browser
-          objectURL = URL.createObjectURL(blob);
+          var objectURL = URL.createObjectURL(blob);
           // invoke showProduct
           showProduct(objectURL, product);
         });
