e57a7fef386c07ad1428cb8a04ce3b40f6b9c509
Chris Mills <cmills@mozilla.com>
2018-09-19 09:58:57 +0100

    fix error in silly story generator finished version

--- javascript/introduction-to-js-1/assessment-finished/main.js
+++ javascript/introduction-to-js-1/assessment-finished/main.js
@@ -34,7 +34,7 @@ function result() {
   if(document.getElementById("uk").checked) {
     var weight = Math.round(300*0.0714286) + ' stone';
     var temperature =  Math.round((94-32) * 5 / 9) + ' centigrade';
-    newStory = newStory.replace('94 farenheit',temperature);
+    newStory = newStory.replace('94 fahrenheit',temperature);
     newStory = newStory.replace('300 pounds',weight);
   }
 
