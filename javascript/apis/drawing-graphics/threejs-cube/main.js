4728a9d7d52654e08769cc3f4dd5bac9ad632b98
Chris Mills <cmills@mozilla.com>
2019-11-04 18:44:56 +0000

    updating Three.js examples to use modern ES features and up-to-date APIs

--- javascript/apis/drawing-graphics/threejs-cube/main.js
+++ javascript/apis/drawing-graphics/threejs-cube/main.js
@@ -1,33 +1,33 @@
-var scene = new THREE.Scene();
+const scene = new THREE.Scene();
 
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
 
-var renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var cube;
+let cube;
 
-var loader = new THREE.TextureLoader();
+let loader = new THREE.TextureLoader();
 
 loader.load( 'metal003.png', function (texture) {
 	texture.wrapS = THREE.RepeatWrapping;
 	texture.wrapT = THREE.RepeatWrapping;
 	texture.repeat.set(2, 2);
 
-	var geometry = new THREE.BoxGeometry(2.4,2.4,2.4);
-	var material = new THREE.MeshLambertMaterial( { map: texture, shading: THREE.FlatShading } );
+	let geometry = new THREE.BoxGeometry(2.4,2.4,2.4);
+	let material = new THREE.MeshLambertMaterial( { map: texture, shading: THREE.FlatShading } );
 	cube = new THREE.Mesh(geometry, material);
 	scene.add(cube);
 
 	draw();
 });
 
-var light = new THREE.AmbientLight('rgb(255,255,255)'); // soft white light
+let light = new THREE.AmbientLight('rgb(255,255,255)'); // soft white light
 scene.add(light);
 
-var spotLight = new THREE.SpotLight('rgb(255,255,255)');
+let spotLight = new THREE.SpotLight('rgb(255,255,255)');
 spotLight.position.set( 100, 1000, 1000 );
 spotLight.castShadow = true;
 scene.add(spotLight);
--- javascript/apis/drawing-graphics/threejs-video-cube/scripts/main.js
+++ javascript/apis/drawing-graphics/threejs-video-cube/scripts/main.js
@@ -1,36 +1,23 @@
-// fork getUserMedia for multiple browser versions
-
-navigator.getUserMedia = ( navigator.getUserMedia ||
-                       navigator.webkitGetUserMedia ||
-                       navigator.mozGetUserMedia ||
-                       navigator.msGetUserMedia);
-
 //main block for doing the video recording
 
-if (navigator.getUserMedia) {
-   console.log('getUserMedia supported.');
-   navigator.getUserMedia (
-      // constraints - only video needed for this app
-      {
-         audio: false,
-         video: true
-      },
+if (navigator.mediaDevices.getUserMedia) {
+  let constraints = {
+     audio: false,
+     video: true
+  }
 
-      // Success callback
-      function(stream) {
-        var video = document.createElement('video');
-        video.srcObject = stream;
-        video.onloadedmetadata = function() {
-          video.play();
-          threeRender(video);
-        };
-      },
-
-      // Error callback
-      function(err) {
-         console.log('The following gUM error occured: ' + err);
-      }
-   );
+  navigator.mediaDevices.getUserMedia(constraints)
+  .then(function(stream) {
+    const video = document.createElement('video');
+    video.srcObject = stream;
+    video.onloadedmetadata = function() {
+      video.play();
+      threeRender(video);
+    };
+  })
+  .catch(function(err) {
+    console.log('The following gUM error occured: ' + err);
+  });
 } else {
    console.log('getUserMedia not supported on your browser!');
 }
@@ -39,30 +26,30 @@ if (navigator.getUserMedia) {
 
 function threeRender(video) {
 
-  var scene = new THREE.Scene();
+  const scene = new THREE.Scene();
 
-  var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-  var renderer = new THREE.WebGLRenderer();
+  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  const renderer = new THREE.WebGLRenderer();
 
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild( renderer.domElement );
 
   // load a texture, set wrap mode to repeat
-  var texture = new THREE.Texture(video);
+  let texture = new THREE.Texture(video);
   texture.wrapS = THREE.RepeatWrapping;
   texture.wrapT = THREE.RepeatWrapping;
   texture.repeat.set( 1, 1 );
 
-  var geometry = new THREE.BoxGeometry(3,3,3);
-  var material = new THREE.MeshLambertMaterial( { map: texture, shading: THREE.FlatShading } );
-  var cube = new THREE.Mesh( geometry, material );
+  let geometry = new THREE.BoxGeometry(3,3,3);
+  let material = new THREE.MeshLambertMaterial( { map: texture, shading: THREE.FlatShading } );
+  let cube = new THREE.Mesh( geometry, material );
   scene.add( cube );
 
   camera.position.x = 0;
   camera.position.y = 0;
   camera.position.z = 5;
 
-  var light = new THREE.AmbientLight( 'rgb(100,100,100)' ); // soft white light
+  let light = new THREE.AmbientLight( 'rgb(100,100,100)' ); // soft white light
   scene.add( light );
 
   // White directional light at half intensity shining from the top.
@@ -71,7 +58,7 @@ function threeRender(video) {
   //scene.add( directionalLight );
 
   // white spotlight shining from the side, casting shadow
-  var spotLight = new THREE.SpotLight( 'rgb(255,255,255)' );
+  let spotLight = new THREE.SpotLight( 'rgb(255,255,255)' );
   spotLight.position.set( 100, 1000, 1000 );
   spotLight.castShadow = true;
   spotLight.shadowMapWidth = 1024;
@@ -96,7 +83,7 @@ function threeRender(video) {
 
   // keyboard controls
 
-  var body = document.querySelector('body');
+  const body = document.querySelector('body');
 
   body.onkeydown = function(e) {
     // 37 is arrow left, 39 is arrow right,
