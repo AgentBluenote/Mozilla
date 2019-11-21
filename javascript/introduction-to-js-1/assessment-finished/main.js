a8fb469fafbd32ebe24e912177e1a056c5b6103f
Chris Mills <cmills@mozilla.com>
2019-11-21 19:38:31 +0000

    updating all examples in JS first steps to use const and var

--- javascript/introduction-to-js-1/assessment-finished/main.js
+++ javascript/introduction-to-js-1/assessment-finished/main.js
@@ -1,25 +1,25 @@
-var customName = document.getElementById('customname');
-var randomize = document.querySelector('.randomize');
-var story = document.querySelector('.story');
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
 
 function randomValueFromArray(array){
-  var random = Math.floor(Math.random()*array.length);
+  const random = Math.floor(Math.random()*array.length);
   return array[random];
 }
 
-var storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-var insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
-var insertY = ['the soup kitchen','Disneyland','the White House'];
-var insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
+let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+let insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
+let insertY = ['the soup kitchen','Disneyland','the White House'];
+let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
 
 randomize.addEventListener('click', result);
 
 function result() {
-  var newStory = storyText;
+  let newStory = storyText;
 
-  var xItem = randomValueFromArray(insertX);
-  var yItem = randomValueFromArray(insertY);
-  var zItem = randomValueFromArray(insertZ);
+  let xItem = randomValueFromArray(insertX);
+  let yItem = randomValueFromArray(insertY);
+  let zItem = randomValueFromArray(insertZ);
 
   newStory = newStory.replace(':insertx:',xItem);
   newStory = newStory.replace(':insertx:',xItem);
@@ -27,13 +27,13 @@ function result() {
   newStory = newStory.replace(':insertz:',zItem);
 
   if(customName.value !== '') {
-    var name = customName.value;
+    const name = customName.value;
     newStory = newStory.replace('Bob',name);
   }
 
   if(document.getElementById("uk").checked) {
-    var weight = Math.round(300*0.0714286) + ' stone';
-    var temperature =  Math.round((94-32) * 5 / 9) + ' centigrade';
+    const weight = Math.round(300*0.0714286) + ' stone';
+    const temperature =  Math.round((94-32) * 5 / 9) + ' centigrade';
     newStory = newStory.replace('94 fahrenheit',temperature);
     newStory = newStory.replace('300 pounds',weight);
   }
